bcce67f146e87adcfb50ce1ebf963ec3c87c69b1
MeStessa <72699640+MeStessa@users.noreply.github.com>
2021-01-22 19:58:25 +0100

    updated styling

--- src/Conditions.js
+++ src/Conditions.js
@@ -10,25 +10,27 @@ export default function Conditions(props){
     <h1 className="city"> {props.data.city}</h1> 
      <button className="btn  current-button" type="submit" ><i class="fas fa-map-pin"></i></button>
     <div className="float-left">
-
+  
             <RecieveDate date={props.data.date}/>
  </div>
+ <ShowUnits celsius={props.data.temp}/>
  <div className="row">
- <div className=" CurrentIcon row col-sm-5">
-             <WeekIcon code={props.data.icon}/> </div>
-             <div className="row col-sm-7">
-             <ShowUnits celsius={props.data.temp}/>
-    </div></div>
-     <div className="minMax">
+    
+       <div className="minMax">
      <span><i class="fas fa-arrow-up"></i> </span> {Math.round(props.data.temp_max)}°
  / <span><i class="fas fa-arrow-down"></i> </span> {Math.round(props.data.temp_min)}° 
     </div>
+    </div>
+    <div className=" CurrentIcon row col-sm">
+             <WeekIcon code={props.data.icon}/>  {props.data.description}</div>
+  
               <div className=" col-6">
              
  </div>
 <ul classname="row">
  <li className="description">
  <div className="float-right col">
+
   </div> <i class="fas fa-thermometer-quarter"></i> {""}
    Feels like {Math.round(props.data.feelslike)} C°
   </li>
